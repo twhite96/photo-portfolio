05bd9c17f73acffcd4356dbaff44e5bceeb3e6cf
Tiffany White <twhite96@users.noreply.github.com>
2019-04-29 02:54:28 -0400

    update config

--- config/index.js
+++ config/index.js
@@ -1,19 +1,19 @@
 module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
 
-  siteTitle: 'Jodie', // Navigation and Site Title
-  siteTitleAlt: 'Jodie - Gatsby Starter Portfolio', // Alternative Site title for SEO
-  siteTitleShort: 'Jodie', // short_name for manifest
-  siteHeadline: 'Come & Enjoy our excellent photos', // Headline for schema.org JSONLD
-  siteUrl: 'https://jodie.lekoarts.de', // Domain of your site. No trailing slash!
+  siteTitle: 'Tiffany R. White Photo', // Navigation and Site Title
+  siteTitleAlt: 'Tiffany White Photography Portfolio', // Alternative Site title for SEO
+  siteTitleShort: 'T.R. White Photo', // short_name for manifest
+  siteHeadline: 'iPhoneography and traditional photography of the Susquehanna Valley', // Headline for schema.org JSONLD
+  siteUrl: 'https://trwhitephoto.netlify.com', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   siteLogo: '/logos/logo.png', // Used for SEO and manifest
   siteDescription: 'Image-heavy photography portfolio with colorful accents & great typography',
-  author: 'LekoArts', // Author for schema.org JSONLD
+  author: 'T.R White Photo', // Author for schema.org JSONLD
 
   // siteFBAppID: '123456789', // Facebook App ID - Optional
-  userTwitter: '@jodie', // Twitter Username
-  ogSiteName: 'jodie', // Facebook Site Name
+  userTwitter: '@tiffanywhitedev', // Twitter Username
+  ogSiteName: 'Tiffany R. White Photo', // Facebook Site Name
   ogLanguage: 'en_US', // og:language
   googleAnalyticsID: 'UA-XXXXXX-X',
 
